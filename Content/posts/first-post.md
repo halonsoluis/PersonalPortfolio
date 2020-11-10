707d82163ee8eb85f80f83b481daf90a9b8dedd9
Hugo Alonso Luis <hugo.alonso@zappware.com>
2020-11-10 23:36:45 +0100

    Initial Publish base content

---
+++ Content/posts/first-post.md
@@ -0,0 +1,8 @@
+---
+date: 2020-11-10 23:24
+description: A description of my first post.
+tags: first, article
+---
+# My first post
+
+My first post's text.
---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPersonalPortfolio",
+    products: [
+        .executable(
+            name: "SwiftPersonalPortfolio",
+            targets: ["SwiftPersonalPortfolio"]
+        )
+    ],
+    dependencies: [
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.6.0")
+    ],
+    targets: [
+        .target(
+            name: "SwiftPersonalPortfolio",
+            dependencies: ["Publish"]
+        )
+    ]
+)
